2c4929d78a564418b4e8ece25a9c1a84f14bfb86
Full Stack Software Developer & Web & Mobile & Blockchain <lovetallman426@gmail.com>
2022-10-27 12:36:29 -0400

    bring it up

---
+++ lib/widget/background.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue.shade50, Colors.blue.shade500])),
+    );
+  }
+}
